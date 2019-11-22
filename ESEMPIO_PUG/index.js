d9896dde020b9d71bba1d1af613d8dac9c9d1dee
marco-tataranni <tataranni.marco@iisgalvanimi.edu.it>
2019-11-22 11:26:02 +0000

    pug completo

--- ESEMPIO_PUG/index.js
+++ ESEMPIO_PUG/index.js
@@ -15,6 +15,13 @@ app.get('/', function (req, res) {
  }); //Dico a express di processare e inviare la pagina index.pug
   //res.send('Hello World!');
 });
+app.get('/profile', (req, res) => {
+  const person = people.profiles.find((p) => p.id === req.query.id);
+  res.render('profile', {
+    title: `About ${person.firstname} ${person.lastname}`,
+    person,
+  });
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
